5ad4ba078ab712b87217a5821cf6e185440b0b56
Amro Nasser <amro.nasser.al@gmail.com>
2026-07-07 17:48:41 +0300

    Update config to use Railway environment variables

--- config.py
+++ config.py
@@ -1,3 +1,13 @@
-TOKEN = "8806198611:AAFlzQpYZ1pQ-T5IAKYgWClXrxYDPCaYxOs"
+import os
 
-ADMIN_CHAT_ID = 1027957590
+TOKEN = os.getenv("BOT_TOKEN") or os.getenv("TOKEN")
+
+if not TOKEN:
+    raise RuntimeError(
+        "BOT_TOKEN/TOKEN is missing in Railway Variables."
+    )
+
+ADMIN_CHAT_ID = int(os.getenv("ADMIN_CHAT_ID", "1027957590"))
+
+GOOGLE_SHEET_ID = os.getenv("GOOGLE_SHEET_ID")
+GOOGLE_SERVICE_ACCOUNT_JSON = os.getenv("GOOGLE_SERVICE_ACCOUNT_JSON")
